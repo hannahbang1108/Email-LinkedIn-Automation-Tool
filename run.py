9f3598d1519ed00c9087f6a2196c87097c92f339
Hannah Bang <65321838+hannahbang1108@users.noreply.github.com>
2025-08-31 16:30:10 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 # library to run the app
 from app import app
+# Import the function from the library
+from dotenv import load_dotenv
+
+# Call the function to load environment variables from the .env file
+load_dotenv()
 
 #runs the app
 if __name__ == '__main__':
+
     app.run()
